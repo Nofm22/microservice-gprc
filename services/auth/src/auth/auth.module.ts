1396c09208c1d782aeed2617552bfb55b3252e2f
NhatMinh <minhphan113@gmail.com>
2023-08-22 15:49:07 +0700

    change package version auth service

--- services/auth/src/auth/auth.module.ts
+++ services/auth/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './service/auth.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
--- services/auth/src/auth/service/auth.service.ts
+++ services/auth/src/auth/service/auth.service.ts
@@ -3,11 +3,19 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { RegisterRequestDto } from '../auth.dto';
 
 import { RegisterResponse } from '../auth.pb';
+import { PrismaService } from '../../prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
   public async register({ email, password }: RegisterRequestDto): Promise<RegisterResponse> {
-    console.log('helloworld');
+    const user = await this.prismaService.users.create({
+      data: {
+        email,
+        password,
+      },
+    });
+    console.log(user);
     return { status: HttpStatus.CREATED, error: null };
   }
 }
