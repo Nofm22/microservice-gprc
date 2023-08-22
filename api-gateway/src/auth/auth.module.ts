20faf01179324d0ff41fe824a726991443510f52
NhatMinh <minhphan113@gmail.com>
2023-08-22 15:19:24 +0700

    set up prisma

--- api-gateway/src/auth/auth.module.ts
+++ api-gateway/src/auth/auth.module.ts
@@ -3,19 +3,36 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthController } from './auth.controller';
 import { AUTH_SERVICE_NAME, AUTH_PACKAGE_NAME } from './auth.pb';
 import { AuthService } from './auth.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Global()
 @Module({
   imports: [
-    ClientsModule.register([
+    // ClientsModule.register([
+    //   {
+    //     name: AUTH_SERVICE_NAME,
+    //     transport: Transport.GRPC,
+    //     options: {
+    //       url: '0.0.0.0:50051',
+    //       // url: process.env.AUTH_CLIENT_URL,
+    //       // url: 'dns:///auth:50051',
+    //       package: AUTH_PACKAGE_NAME,
+    //       protoPath: 'node_modules/grpc-nest-proto/proto/auth.proto',
+    //     },
+    //   },
+    // ]),
+    ClientsModule.registerAsync([
       {
+        imports: [ConfigModule],
         name: AUTH_SERVICE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          url: 'dns:///auth:50051',
-          package: AUTH_PACKAGE_NAME,
-          protoPath: 'node_modules/grpc-nest-proto/proto/auth.proto',
-        },
+        useFactory: async (config: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: config.get<string>('AUTH_CLIENT_URL'),
+            package: AUTH_PACKAGE_NAME,
+            protoPath: 'node_modules/grpc-nest-proto/proto/auth.proto',
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
