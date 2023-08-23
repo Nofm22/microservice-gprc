9e9979117a20f02bfd2ada884f3490dc93f913d8
NhatMinh <minhphan113@gmail.com>
2023-08-23 09:34:51 +0700

    update env file

--- api-gateway/src/auth/auth.module.ts
+++ api-gateway/src/auth/auth.module.ts
@@ -8,35 +8,35 @@ import { join } from 'path';
 @Global()
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: AUTH_SERVICE_NAME,
-        transport: Transport.GRPC,
-        options: {
-          url: '0.0.0.0:50051',
-          // url: process.env.AUTH_CLIENT_URL,
-          // url: 'dns:///auth:50051',
-          package: AUTH_PACKAGE_NAME,
-          protoPath: join(__dirname, './auth.proto'),
-        },
-      },
-    ]),
-    // ClientsModule.registerAsync([
+    // ClientsModule.register([
     //   {
-    //     imports: [ConfigModule],
     //     name: AUTH_SERVICE_NAME,
-    //     useFactory: async (config: ConfigService) => ({
-    //       transport: Transport.GRPC,
-    //       options: {
-    //         url: config.get<string>('AUTH_CLIENT_URL'),
-    //         package: AUTH_PACKAGE_NAME,
-    //         // protoPath: 'node_modules/gitRepo/proto/auth.proto',,
-    //         protoPath: './auth.proto',
-    //       },
-    //     }),
-    //     inject: [ConfigService],
+    //     transport: Transport.GRPC,
+    //     options: {
+    //       url: '0.0.0.0:50051',
+    //       // url: process.env.AUTH_CLIENT_URL,
+    //       // url: 'dns:///auth:50051',
+    //       package: AUTH_PACKAGE_NAME,
+    //       protoPath: join(__dirname, './auth.proto'),
+    //     },
     //   },
     // ]),
+    ClientsModule.registerAsync([
+      {
+        imports: [ConfigModule],
+        name: AUTH_SERVICE_NAME,
+        useFactory: async (config: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: config.get<string>('AUTH_CLIENT_URL'),
+            package: AUTH_PACKAGE_NAME,
+            // protoPath: 'node_modules/gitRepo/proto/auth.proto',,
+            protoPath: join(__dirname, './auth.proto'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
